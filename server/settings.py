54ab32a28cb0ed08c69210c1c0221121c9664812
wickey0405 <wickey0405.11028349@gmail.com>
2022-01-29 12:49:09 +0900

    heroku migirate try change option

--- server/settings.py
+++ server/settings.py
@@ -139,7 +139,7 @@ try:
             'HOST': url.hostname,
             'PORT': url.port,
             'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'init_command': "SET sql_mode='STRICT_ALL_TABLES'",
             },
         })
 
